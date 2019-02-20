be3c2fb058a4baa6264da819d4c2fe3046e86280
Brendan Berejikian <bezoric1@gmail.com>
2019-02-20 12:41:49 -0500

    Update Program.cs

--- QuadraticFormula/Program.cs
+++ QuadraticFormula/Program.cs
@@ -10,6 +10,16 @@ namespace QuadraticFormula
     {
         static void Main(string[] args)
         {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double sqrtOp = b * b - 4 * a * c;
+            double x1 = (-b + Math.Sqrt(sqrtOp)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(sqrtOp)) / (2 * a);
+            Console.WriteLine("Real Solutions" + x1 , x2);
+            Console.ReadKey();
+
         }
     }
 }
